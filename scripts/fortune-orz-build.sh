b658114dc26afaa61953d72159461f7110cc4ec3
Icenowy Zheng <icenowy@outlook.com>
2016-02-25 03:33:39 -0500

    fortune-orz-build.sh: fix

--- scripts/fortune-orz-build.sh
+++ scripts/fortune-orz-build.sh
@@ -45,10 +45,13 @@ EOF
 make && sudo make install && sudo rm -rf /opt/fortune-orz/share/fortune
 [ ! -d fortune-orz ] && git clone https://github.com/Icenowy/fortune-orz
 sudo mkdir -p /opt/fortune-orz/share/fortune/
-sudo cp fortune-orz/* /opt/fortune-orz/share/fortune/
+sudo cp -r fortune-orz/* /opt/fortune-orz/share/fortune/
 sudo rm -f /opt/fortune-orz/share/fortune/README.md
-sudo rm -f /opt/fortune-orz/share/fortune/fortune-orz-build.sh
+sudo rm -f /opt/fortune-orz/share/fortune/LICENSE*
+sudo rm -rf /opt/fortune-orz/share/fortune/scripts
 for i in /opt/fortune-orz/share/fortune/*
 do
 sudo /opt/fortune-orz/bin/strfile $i
 done
+ln -sv orizon /opt/fortune-orz/share/fortune/fortunes
+ln -sv orizon.dat /opt/fortune-orz/share/fortune/fortunes.dat
